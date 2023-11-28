a01f4ff1741f923182456bea03164822ed599f32
Christopher Acosta <chrisacosta12345@gmail.com>
2023-11-28 15:51:41 -0800

    added json file, css, bootstrap

--- src/components/HornedBeast/HornedBeast.jsx
+++ src/components/HornedBeast/HornedBeast.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function HornedBeast(props){
-    return(
+
+function HornedBeast(props) {
+
+    const [vote, setVote] = useState(0);
+
+    function increment() {
+        setVote(vote + 1)
+    }
+
+    return (
         <>
-            <h2>{props.title}</h2>
-            <img src={props.imageUrl} alt={props.keyword}/>
-            <p>{props.description}</p>
+            <Card style={{ width: '18rem', margin: '5px', borderStyle: 'groove', borderWidth: '2px', borderColor: 'blue'}}>
+                <Card.Img variant="top" src={props.imageUrl} alt= {props.keyword}/>
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>
+                    {props.description}
+                    </Card.Text>
+                    <Button onClick={increment} variant="primary">Like: {vote}</Button>
+                </Card.Body>
+            </Card>
         </>
 
     )
